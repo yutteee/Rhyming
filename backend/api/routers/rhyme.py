d0621ab96b8db45f9ae88066c429d87e8aca0b98
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-01 01:22:43 +0900

    :+1: 韻判定エンドポイントの型定義 #2

--- backend/api/routers/rhyme.py
+++ backend/api/routers/rhyme.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+import api.schemas.rhyme as rhyme_schema
 
 router = APIRouter()
 
-@router.post("/judge")
-async def judge_rhyme():
-    pass
+@router.post("/judge", response_model=rhyme_schema.RhymeScore)
+async def judge_rhyme(request_body: rhyme_schema.RhymeInput):
+    return rhyme_schema.RhymeScore(score=8, title="断頭台", input="パッとしない")
