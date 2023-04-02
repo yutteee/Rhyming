d4efa953c7828de9298a44220875d4fcb50c2991
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-03 00:57:36 +0900

    :+1: pykakasiで分かち書き #2

--- backend/api/routers/rhyme.py
+++ backend/api/routers/rhyme.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+import pykakasi
 import api.schemas.rhyme as rhyme_schema
+
+kks = pykakasi.kakasi()
 
 router = APIRouter()
 
 @router.post("/judge", response_model=rhyme_schema.RhymeScore)
 async def judge_rhyme(request_body: rhyme_schema.RhymeInput):
+    title = kks.convert(request_body.title)
+    print(''.join([item["hepburn"] for item in title]))
+    input = kks.convert(request_body.input)
+    print(''.join([item["hepburn"] for item in input]))
     return rhyme_schema.RhymeScore(score=8, title="断頭台", input="パッとしない")
