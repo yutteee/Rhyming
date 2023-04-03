c8ea6d701a01088d5237e9d290af43d122be325e
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-03 22:41:58 +0900

    :+1: 子音踏みを判定 #2

--- backend/api/routers/rhyme.py
+++ backend/api/routers/rhyme.py
@@ -22,16 +22,26 @@ async def judge_rhyme(request_body: rhyme_schema.RhymeInput):
     title = kks.convert(request_body.title)
     input = kks.convert(request_body.input)
 
-    tmpTitle = ''.join([item["hira"] for item in title])
-    tmpInput = ''.join([item["hira"] for item in input])
-    hiraTitleKakasi = kks.convert(ignoreWord(tmpTitle))
-    hiraInputKakasi = kks.convert(ignoreWord(tmpInput))
+    hiraTitle = ''.join([item["hira"] for item in title])
+    hiraInput = ''.join([item["hira"] for item in input])
 
+    filteredHiraTitle = ignoreWord(hiraTitle)
+    filteredHiraInput = ignoreWord(hiraInput)
+
+    # 子音踏みをreturn
+    if filteredHiraTitle == filteredHiraInput:
+        score = len(filteredHiraTitle) + 3
+        return rhyme_schema.RhymeScore(score=score, title=request_body.title, input=request_body.input)
+
+    hiraTitleKakasi = kks.convert(filteredHiraTitle)
+    hiraInputKakasi = kks.convert(filteredHiraInput)
     romajiTitle = ''.join([item["hepburn"] for item in hiraTitleKakasi])
     romajiInput = ''.join([item["hepburn"] for item in hiraInputKakasi])
 
     vowelsTitle = removeConsonants(romajiTitle)
     vowelsInput = removeConsonants(romajiInput)
+
+
     print(vowelsTitle)
     print(vowelsInput)
     return rhyme_schema.RhymeScore(score=8, title=request_body.title, input=request_body.input)
