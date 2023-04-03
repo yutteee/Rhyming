5d9de82a4c0a6537ac9ed1fcb13b58a28b7be8df
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-03 21:12:21 +0900

    :+1: 母音だけを抽出できた #2

--- backend/api/routers/rhyme.py
+++ backend/api/routers/rhyme.py
@@ -6,10 +6,32 @@ kks = pykakasi.kakasi()
 
 router = APIRouter()
 
+def ignoreWord(word):
+    return word.replace("ん", "").replace("ー", "").replace("っ", "")
+
+def removeConsonants(input):
+    vowels = "aiueo"
+    result = ""
+    for char in input:
+        if char in vowels:
+            result += char
+    return result
+
 @router.post("/judge", response_model=rhyme_schema.RhymeScore)
 async def judge_rhyme(request_body: rhyme_schema.RhymeInput):
     title = kks.convert(request_body.title)
-    print(''.join([item["hepburn"] for item in title]))
     input = kks.convert(request_body.input)
-    print(''.join([item["hepburn"] for item in input]))
-    return rhyme_schema.RhymeScore(score=8, title="断頭台", input="パッとしない")
+
+    tmpTitle = ''.join([item["hira"] for item in title])
+    tmpInput = ''.join([item["hira"] for item in input])
+    hiraTitleKakasi = kks.convert(ignoreWord(tmpTitle))
+    hiraInputKakasi = kks.convert(ignoreWord(tmpInput))
+
+    romajiTitle = ''.join([item["hepburn"] for item in hiraTitleKakasi])
+    romajiInput = ''.join([item["hepburn"] for item in hiraInputKakasi])
+
+    vowelsTitle = removeConsonants(romajiTitle)
+    vowelsInput = removeConsonants(romajiInput)
+    print(vowelsTitle)
+    print(vowelsInput)
+    return rhyme_schema.RhymeScore(score=8, title=request_body.title, input=request_body.input)
